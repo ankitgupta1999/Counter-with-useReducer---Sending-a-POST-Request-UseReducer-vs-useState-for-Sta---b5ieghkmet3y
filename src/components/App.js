e7a38bb367ce5729e5bdc8a2f9f7b84ac56347dc
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-06-17 21:57:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,43 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
+
+
+import React, { useReducer } from 'react';
 import '../styles/App.css';
+
+const initialTaskState = {
+  count: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return { count: state.count + 1 };
+    case 'delete':
+      return { count: state.count - 1 };
+    case 'deleteAll':
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+  const [taskState, dispatch] = useReducer(reducer, initialTaskState);
+
   return (
     <div id="main">
-
+      <h2>Task Counter</h2>
+      <button onClick={() => dispatch({ type: 'add' })} id="addTaskBtn">
+        Add a task
+      </button>
+      <button onClick={() => dispatch({ type: 'delete' })} id="delTaskBtn">
+        Delete a task
+      </button>
+      <button onClick={() => dispatch({ type: 'deleteAll' })} id="delAllTaskBtn">
+        Delete all tasks
+      </button>
+      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
