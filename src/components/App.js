b62da24cb7be12bbfe97dfe61a62ce996c6169b0
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-08-02 11:04:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,17 @@
-counter with reducer
-
-Counter reducer
-
 import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
-
-const initialState = {
-  counter:0
-}
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,initialState)
-
-const handleIncre = () =>{
-  dispatch({type: "INCREMENT"});
-}
-
-const hnadleDecre = () =>{
-  dispatch({type : "DECREMENT"})
-}
-
-
+const [state,dispatch] = useReducer(counterReducer, 0)
   return (
     <div id="main">
-       <span id='counter'>{state.counter}</span>
-      <button id='increment-btn' onClick={handleIncre}>Increment</button>
-      <button id='decrement-btn' onClick={hnadleDecre}>Decrement</button>
-
+      <span id="counter">{state}</span>
+      <button id="increment-btn" onClick={() => dispatch({ type: 'increment' })}>
+        Increment
+      </button>
+      <button id="decrement-btn" onClick={() => dispatch({ type: 'decrement' })}>
+        Decrement
+      </button>
     </div>
   )
 }
